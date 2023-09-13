6580038e29be37e0eee22b34656b30368fd90dc1
Harsha <harsha9390459294@gmail.com>
2023-09-13 08:42:09 +0530

    Added Docker related code..so as to execute in container

--- src/test/java/TestCases/TestCasesBaseClass.java
+++ src/test/java/TestCases/TestCasesBaseClass.java
@@ -2,6 +2,8 @@ package TestCases;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -12,19 +14,33 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestCasesBaseClass {
 	
-	public static WebDriver driver; ///this method need not be static...however, made static as required for a class in report generation or something...check
+	//public static RemoteWebDriver driver; ///this method need not be static...however, made static as required for a class in report generation or something...check
+	public static WebDriver driver;
 	public ResourceBundle rb;
 	
 	@BeforeClass
-	public void setup() throws InterruptedException{
+	public void setup() throws InterruptedException, MalformedURLException{
 		
 		rb = ResourceBundle.getBundle("config");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\TechGeek\\Desktop\\Softwares\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		
+		//ChromeOptions chromeOptions = new ChromeOptions();
+		//chromeOptions.setCapability("browserVersion", "116.0");
+		
+		//URL url = new URL("http://localhost:4444/wd/hub");
+		//driver = new RemoteWebDriver(url,chromeOptions);
+		
 		driver = new ChromeDriver();
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get(rb.getString("appUrl"));
 		driver.manage().window().maximize();
---
+++ src/test/java/TestCases/test1.java
@@ -0,0 +1,16 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import PageObjects.HomePage;
+import PageObjects.ResultsPage;
+
+public class test1 {
+	
+	@Test
+	public void roughWork() {
+		
+		System.out.println("HelloWorld");
+	}
+
+}
